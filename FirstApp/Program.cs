205e1d7d3170c34c6e407454ce91bd5a9c4b2bf0
vitaxaov <johndoe@example.com>
2024-03-19 21:13:44 +0300

    123

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,14 +1,16 @@
-﻿Console.WriteLine("Input your name");
-string name = Console.ReadLine();
+﻿Console.WriteLine("Input123213 your name");
+string name = Console.R123213eadLine();
 
-Console.WriteLine($"Hello {name}");
+Console.WriteLine($"Hel213123lo {name}");
 
-Console.WriteLine($"Helcxxclo {name}");
+Console.WriteLine($"Helc213123xxclo {name}");
 
-Console.WriteLine($"Helcxxclfsdfsdfgfgo {name}");
-
+Console.WriteLine($"Helcxx123123clfsdfsdfgfgo {name}");
+123213
+Console.WriteLine($"Helcxsd12312fsdfxclo {name}");
+Console.WriteLine($"Helcxsdfsdfxclo {name}");
+Console.WriteLine($"Helcxsd123213fsdfxclo {name}");
 Console.WriteLine($"Helcxsdfsdfxclo {name}");
 Console.WriteLine($"Helcxsdfsdfxclo {name}");
-Console.WriteLine($"Helcxsdfsdfxclo {name}");
-Console.WriteLine($"Helcxsdfsdfxclo {name}");
+Console.WriteLine($"Helcxsdf123123sdfxclo {name}");
 Console.WriteLine($"Helcxsdfsdfxclo {name}");
